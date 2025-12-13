dc436811b23ce563f348e94616825b84555a3b29
Neo Andersson <neoandersson321@gmail.com>
2025-12-13 16:17:18 +0100

    Lite updateringar på attack-systemet

--- BaseBuilder/Scripts/Player/player.gd
+++ BaseBuilder/Scripts/Player/player.gd
@@ -4,16 +4,18 @@ const MAX_SPEED = 500.0
 const ACC = 7.0
 
 var input: Vector2
+var can_attack: bool  =true
 
 var direction_history = []
-const HISTORY_LENGHT = 10
-const ATTACK_ANGLE_THRESHOLD = PI/2
-
+const HISTORY_LENGHT = 7
+const MIN_ATTACK_ANGLE = PI/2
+const MAX_ATTACK_ANGLE = 4*PI/5
 var weapon_scenes = {
 	"1": preload("res://Scenes/Player/pickaxe.tscn")
 }
 
 @onready var weapon_slot = $Weapon
+@onready var attack_timer = $AttackTimer
 
 func _get_move_input():
 	input.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
@@ -21,12 +23,10 @@ func _get_move_input():
 	return input.normalized()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("LeftClick"):
-		attack_init()
-	
 	for key in weapon_scenes.keys():
 		if event.is_action_pressed(key):
 			equip_weapon(weapon_scenes[key])
+
 
 func _physics_process(delta: float) -> void:
 	var player_input = _get_move_input()
@@ -53,3 +53,24 @@ func equip_weapon(weapon_scene):
 func attack_init():
 	if weapon_slot.get_child_count() > 0:
 		weapon_slot.get_child(0).attack()
+		can_attack = false
+		attack_timer.start()
+
+func store_direction(dir):
+		direction_history.append(dir)
+		if direction_history.size() > HISTORY_LENGHT:
+			direction_history.pop_front()
+
+func check_for_attack(dir):
+	if ! can_attack:
+		return
+	if direction_history.size() < HISTORY_LENGHT:
+		return
+	var old_dir = direction_history[0]
+	var angle = old_dir.angle_to(dir)
+	if angle <= -MIN_ATTACK_ANGLE and -MAX_ATTACK_ANGLE <= angle:
+		attack_init()
+
+
+func _on_attack_timer_timeout() -> void:
+	can_attack = true
--- BaseBuilder/Scripts/Player/weapon_base.gd
+++ BaseBuilder/Scripts/Player/weapon_base.gd
@@ -1,4 +1,12 @@
 extends Area2D
 
+var damage_window: bool = false
+@export var weapon_damage: int
 func attack():
-	print("attack")
+	damage_window = true
+	await get_tree().create_timer(0.5).timeout
+	damage_window = false
+
+func _on_body_entered(body: CharacterBody2D):
+	if body.is_in_group("enemies") and damage_window:
+		body._take_damage(weapon_damage)
